9f00fa0f930852fa7aa0f9b6854e496b5eb8fe25
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 04:38:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def name(dog_name)
+    @this_is_the_dogs_name = dog_name
+  end
+
+  def name
+    @this_is_the_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
